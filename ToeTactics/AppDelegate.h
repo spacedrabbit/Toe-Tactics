d29ae1440cfa9ccd44eb5acb5b5715fbcf0f8ace
spacedrabbit <louis.tur@gmail.com>
2014-04-27 16:53:56 -0400

    Initial Commit

---
+++ ToeTactics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToeTactics
+//
+//  Created by Louis Tur on 4/27/14.
+//  Copyright (c) 2014 Louis Tur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToeTacticsTests/ToeTacticsTests.m
@@ -0,0 +1,34 @@
+//
+//  ToeTacticsTests.m
+//  ToeTacticsTests
+//
+//  Created by Louis Tur on 4/27/14.
+//  Copyright (c) 2014 Louis Tur. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToeTacticsTests : XCTestCase
+
+@end
+
+@implementation ToeTacticsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
